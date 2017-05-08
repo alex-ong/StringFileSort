721de3163dece6f1da18757e7ceccf40525afaaa
alex-ong <alex.ong@unsw.edu.au>
2017-05-08 14:06:49 +1000

    Added command line usage for randomgen

--- randomgen.py
+++ randomgen.py
@@ -12,12 +12,17 @@ choices = string.ascii_uppercase
 def gen_strings(numStrings, lenString=60):
     return ''.join((''.join(random.choice(choices) for _ in range(lenString)) + '\n') for x in range(numStrings))
 
-if __name__ == '__main__':
-    gigs = 0.25    
-    stringLength = 60
+if __name__ == '__main__':    
+    if len(sys.argv) < 3:
+        print ('usage: randomgen.py NUM_GIGS STRING_LENGTH')
+        sys.exit()
+        
+    gigs = float(sys.argv[1])
+    stringLength = int(sys.argv[2])
     totalStrings = int(gigs*1024*1024*1024 / (stringLength + 1))
     numStrings = 0
         
+    print ("Generating %f gigabytes of data, string length %d" % (gigs,stringLength))
     p = Pool(4)
     start = time.time()
     with open('rawblob.blob', 'w') as f:
