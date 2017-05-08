7a3e2c4907122c105ff3be2b585fa2a0f8372371
alex-ong <alex.ong@unsw.edu.au>
2017-05-08 12:15:50 +1000

    PythonSorter multi-thread is now finished

--- pythonsorter.py
+++ pythonsorter.py
@@ -2,7 +2,12 @@ import string
 from multiprocessing import Pool
 import os
 import time
-choices = string.ascii_uppercase + string.ascii_lowercase
+import shutil
+
+choices = string.ascii_uppercase
+
+import prettyprint
+
 
 def sort_file_one_arg(arg):
     fileName,start_byte,num_bytes,procID = arg
@@ -27,7 +32,7 @@ def sort_file(fileName,start_byte,num_bytes,procID):
     
     files = {}
     for char in choices:
-        f = open(char + str(procID) + '.txt', 'a+')
+        f = open(getFile(char,procID), 'a+')
         files[char] = f
         
     for entry in allEntries:        
@@ -38,7 +43,58 @@ def sort_file(fileName,start_byte,num_bytes,procID):
     for file in files.values():
         file.close()
         
+def sort_completed_one_arg(args):
+    procID, char = args
+    sort_completed(procID, char)
+    
+def sort_completed(procID, char):         
+    f = open(getFile(char,procID), 'r')
+    lines = f.readlines()
+    lines.sort()
+    f.close()
+    f = open(getFile(char,procID), 'w')
+    f.writelines(lines)
+    
+def getFile(char,procID):
+    return char + str(procID) + '.txt'
+    
+def merge_sorted(char, numProcs):
+    files = []
+    for i in range (numProcs):
+        f = open(getFile(char,i))
+        files.append(f)
+    
+    final = open(getFile(char,'_final'),'w')
+    
+    currentCmp = []
+    for file in files:
+        currentCmp.append(file.readline().strip())
         
+    while len(files) > 0:        
+        minimum = min(currentCmp)
+        index = currentCmp.index(minimum)
+        final.write(minimum + '\n')
+        
+        newValue = files[index].readline()
+        if not newValue:
+            files[index].close()
+            del files[index]
+            del currentCmp[index]
+        else:
+            currentCmp[index] = newValue.strip()
+    
+    for fileName in (getFile(char, i) for i in range (numProcs)):
+        os.remove(fileName)
+        
+def merge_final(char):
+    final = open('sorted.blob','a+')
+    merger = open(getFile(char,'_final'),'r')
+    
+    shutil.copyfileobj(merger, final)    
+    
+    final.close()
+    merger.close()
+    
 if __name__ == '__main__':
     p = Pool(4)
     blockSize = 1024*1024*50
@@ -53,5 +109,27 @@ if __name__ == '__main__':
             bytesDone += blockSize
             allArgs.append(args)
             
-        p.map(sort_file_one_arg, allArgs)        
+        p.map(sort_file_one_arg, allArgs) 
+        prettyprint.printProgressBar(bytesDone, filesize, prefix = 'Phase 1:', suffix = 'Complete', length = 50)
+    
+    # do a sort on all the mini-files
+    charIndex = 0
+    for char in choices:
+        p.map(sort_completed_one_arg, ((i,char) for i in range(4)))
+        prettyprint.printProgressBar(charIndex, len(choices), prefix = 'Phase 2:', suffix = 'Complete', length = 50)
+        charIndex += 1
+    
+    # now merge files
+    charIndex = 0
+    for char in choices[1:]:
+        merge_sorted(char,4)
+        prettyprint.printProgressBar(charIndex, len(choices), prefix = 'Phase 3:', suffix = 'Complete', length = 50)
+        charIndex += 1
+        
+    # final merge - one blob.
+    charIndex = 0
+    for char in choices:
+        merge_final(char)
+        prettyprint.printProgressBar(charIndex, len(choices), prefix = 'Final Phase:', suffix = 'Complete', length = 50)
+        charIndex += 1
     
--- randomgen.py
+++ randomgen.py
@@ -5,29 +5,10 @@ import sys
 
 from multiprocessing import Pool
 import itertools
+import prettyprint as printProgressBar
 
-# Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = 'x'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
 
-choices = string.ascii_uppercase + string.ascii_lowercase
+choices = string.ascii_uppercase
 
 def gen_strings(numStrings, lenString=60):
     return ''.join((''.join(random.choice(choices) for _ in range(lenString)) + '\n') for x in range(numStrings))
@@ -48,5 +29,5 @@ if __name__ == '__main__':
             finalResult = ''.join(results)
             f.write(finalResult)
             numStrings += 1024*30*4
-            printProgressBar(numStrings, totalStrings, prefix = 'Progress:', suffix = 'Complete', length = 50)
+            prettyprint.printProgressBar(numStrings, totalStrings, prefix = 'Progress:', suffix = 'Complete', length = 50)
     print ("Complete, took " +str(time.time() - start) + "seconds")
